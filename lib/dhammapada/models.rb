db7054a872ef7566ba5644094322bc2a5f7825fc
Christian Meier <m.kristian@web.de>
2013-01-16 13:28:36 +0530

    added license header

--- lib/dhammapada/models.rb
+++ lib/dhammapada/models.rb
@@ -1,3 +1,23 @@
+#
+# dhammapada app - webapp to browse and display two translations of the
+# Dhammapada.
+# Copyright (C) 2013 Christian Meier
+#
+# This file is part of dhammapada app.
+#
+# dhammapada app is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# dhammapada app is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with dhammapada app.  If not, see <http://www.gnu.org/licenses/>.
+#
 # -*- coding: utf-8 -*-
 
 require 'virtus'
